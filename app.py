d2382cce5a11e6cf88de41a124ce0ac63215767c
gabrielt1101 <92883177+gabrielt1101@users.noreply.github.com>
2022-01-25 18:04:17 -0500

    commit on 'main'

---
+++ app.py
@@ -0,0 +1,73 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    '''A dummy docstring'''
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+    
+    def pub1(self):
+        """a dummy docstring"""
+        print("")
+    
+    def pub2(self):
+        """a dumy docstring"""
+        print("")
+        
+
+
+@app.route("/edit")
+def home1():
+        """A dummy docstring."""
+        todo_list = Todo.query.all()
+        return render_template("base.html", todo_list=todo_list)
+
+
+@app.route('/')
+def list1():
+    """a dummy docstring"""
+    todo_list = Todo.query.all()
+    return render_template("list.html", todo_list=todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    """A dummy docstring"""
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home1"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    """a dummy docstring"""
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home1"))
+
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    """a dummy docstring"""
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home1"))
+
+
+if __name__ == "__main__":
+    db.create_all()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port, debug=True) 
+   
---
+++ appv1.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, redirect, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = flask(__name__)
+
+@app.route("/")
+def home1():
+    return 'hi there'
+
+if __name__ == "__main__":
+    app.run('',8000,debug=True)
